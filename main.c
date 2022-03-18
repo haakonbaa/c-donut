6d6a1bc4107dc8546bb29a51bb1baadbc8cf15d1
Håkon Bårsaune <haakbaa@stud.ntnu.no>
2022-03-18 22:19:58 +0100

    working doughnut

--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-const double r = 1;
-const double dTheta = 0.05;
-const double dPhi = 0.05;
-const double cameraDist = 9;
-const double cameraWall = -1.1;
+const double r = 0.6;
+const double thickness = 0.4;
+const double dTheta = 0.01;
+const double dPhi = 0.01;
+const double cameraDist = 100;
+const double cameraWall = -2;
 
 const char gradient[] = ".,-~:;=!*#$@";
 const int gradlen = 12;
@@ -39,6 +40,9 @@ int main() {
     // set default light angle
     double lightAngleTheta = 0;
     double lightAnglePhi = M_PI * 0.5;
+    // set default rotation angle
+    double rotX = 0;
+    double rotZ = 0;
     while (1) {
         // set default values in all buffers
         memset(buffer, ' ', sizeof(char) * pixels);
@@ -53,10 +57,29 @@ int main() {
         for (double theta = 0; theta < 2 * M_PI; theta += dTheta) {
             for (double phi = 0; phi < M_PI; phi += dPhi) {
                 // position (x,y,z) in 3-dimensions
-                double x = r * cos(theta) * sin(phi);
-                double y = r * sin(theta) * sin(phi);
-                double z = r * cos(phi);
-                // camera x and y position
+                // SPHERE
+                // double x = r * cos(theta) * sin(phi);
+                // double y = r * sin(theta) * sin(phi);
+                // double z = r * cos(phi);
+                // double nx = x / r, ny = y / r, nz = z / r;
+                // DOUGHNUT
+                double x = r * cos(theta) + thickness * cos(theta) * sin(phi);
+                double y = r * sin(theta) + thickness * sin(theta) * sin(phi);
+                double z = thickness * cos(phi);
+                double nx = cos(theta) * sin(phi);
+                double ny = sin(theta) * sin(phi);
+                double nz = cos(phi);
+                // rotate around x then z
+                // double transx = x * cos(rotZ) - y * sin(rotZ) * sin(rotX) + z * sin(rotZ) * sin(rotX);
+                // double transy = x * sin(rotZ) + y * cos(rotZ) * sin(rotX) - z * cos(rotZ) * sin(rotX);
+                // double transz = z * cos(rotX) + y * sin(rotX);
+                double transx = x * cos(rotZ) - y * cos(rotX) * sin(rotZ) + z * sin(rotZ) * sin(rotX);
+                double transy = y * cos(rotZ) * cos(rotX) + x * sin(rotZ) - z * cos(rotZ) * sin(rotX);
+                double transz = z * cos(rotX) + y * sin(rotX);
+                x = transx;
+                y = transy;
+                z = transz;
+                // // camera x and y position
                 double sy = (cameraDist * z) / (cameraDist + y - cameraWall);
                 double sx = (cameraDist * x) / (cameraDist + y - cameraWall);
                 // buffer y and x position
@@ -69,8 +92,6 @@ int main() {
                 // find distance from camera to point squared
                 double distCamera = x * x + (y - cameraWall + cameraDist) * (y - cameraWall + cameraDist) + z * z;
                 if (distCamera < zbuffer[bufferIndex]) {
-                    // find surface normals
-                    double nx = x / r, ny = y / r, nz = z / r;
                     unsigned int light = (unsigned int)(((nx * lx + ny * ly + nz * lz) + 1) * 0.5 * (gradlen - 1) + 0.5);
                     light = max(0, min(light, gradlen - 1));
                     zbuffer[bufferIndex] = distCamera;
@@ -84,10 +105,12 @@ int main() {
             putchar(buffer[i]);
         }
         fflush(stdout);
-        usleep(10000);
+        usleep(50000);
         // update light angle
-        lightAngleTheta += 0.05;
-        lightAnglePhi += 0.0;
+        // lightAngleTheta += 0.05;
+        // lightAnglePhi += 0.0;
+        rotX += 0.1;
+        rotZ += 0.1;
     }
 
     // free buffer
