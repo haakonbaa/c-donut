96588503f2003cf6fe6da0dbca336f3d49979f1a
Håkon Bårsaune <haakbaa@stud.ntnu.no>
2022-03-18 20:13:18 +0100

    change to 1D array

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
@@ -28,17 +29,14 @@ int main() {
 
     unsigned rows = w.ws_row;
     unsigned cols = w.ws_col;
-    // allocate buffer
-    char** buffer = (char**)malloc(sizeof(char*) * rows);
-    for (unsigned int i = 0; i < rows; i++) {
-        buffer[i] = (char*)malloc(sizeof(char) * cols);
-    }
+    const unsigned pixels = rows * cols;
+    // allocate character buffer
+    char* buffer = (char*)malloc(sizeof(char) * pixels);
+    memset(buffer, ' ', sizeof(char) * pixels);
+    // allocate depth buffer
+    float* zbuffer = (float*)malloc(sizeof(float) * pixels);
+    memset(zbuffer, 0, sizeof(float) * pixels);
     // fill buffer with spaces
-    for (unsigned r = 0; r < rows; r++) {
-        for (unsigned c = 0; c < cols; c++) {
-            buffer[r][c] = ' ';
-        }
-    }
     // fill in circle
     for (double theta = 0; theta < 2 * M_PI; theta += dTheta) {
         for (double phi = 0; phi < M_PI; phi += dPhi) {
@@ -52,21 +50,17 @@ int main() {
             int bx = (int)(0.5 * cols + sx * rows + 0.5);
             unsigned int bufferx = max(0, min(bx, cols - 1));
             unsigned int bufferz = max(0, min(bz, rows - 1));
-            buffer[bufferz][bufferx] = '#';
+            buffer[bufferz * cols + bufferx] = '#';
         }
     }
     //  print buffer
-    for (unsigned r = 0; r < rows; r++) {
-        for (unsigned c = 0; c < cols; c++) {
-            putchar(buffer[r][c]);
-        }
+    for (unsigned i = 0; i < pixels; i++) {
+        putchar(buffer[i]);
     }
 
     // free buffer
-    for (unsigned int i = 0; i < rows; i++) {
-        free(buffer[i]);
-    }
     free(buffer);
+    free(zbuffer);
 
     return 0;
 }
