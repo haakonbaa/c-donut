1d35124e3326c27b1cf675300e6e07dbf7a96902
Håkon Bårsaune <haakbaa@stud.ntnu.no>
2022-03-18 20:30:13 +0100

    add distance buffer

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <float.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,8 +9,9 @@
 const double r = 1;
 const double dTheta = 0.01;
 const double dPhi = 0.01;
-const double cameraDist = 1;
+const double cameraDist = 9;
 const double cameraWall = -1.1;
+const double lightAngle = M_PI * 4.0 / 3.0;
 
 const char gradient[] = ".,-~:;=!*#$@";
 const int gradlen = 12;
@@ -35,22 +37,32 @@ int main() {
     memset(buffer, ' ', sizeof(char) * pixels);
     // allocate depth buffer
     float* zbuffer = (float*)malloc(sizeof(float) * pixels);
-    memset(zbuffer, 0, sizeof(float) * pixels);
-    // fill buffer with spaces
+    for (unsigned int i = 0; i < pixels; i++) {
+        zbuffer[i] = FLT_MAX;
+    }
     // fill in circle
     for (double theta = 0; theta < 2 * M_PI; theta += dTheta) {
         for (double phi = 0; phi < M_PI; phi += dPhi) {
+            // position (x,y,z) in 3-dimensions
             double x = r * cos(theta) * sin(phi);
             double y = r * sin(theta) * sin(phi);
             double z = r * cos(phi);
-            double sz = (cameraDist * z) / (cameraDist + y - cameraWall);
+            // camera x and y position
+            double sy = (cameraDist * z) / (cameraDist + y - cameraWall);
             double sx = (cameraDist * x) / (cameraDist + y - cameraWall);
-            int bz = (int)((sz + 1) * 0.5 * rows + 0.5);
-            // characters are double as high as they are wide
+            // buffer y and x position
+            int by = (int)((sy + 1) * 0.5 * rows + 0.5);
             int bx = (int)(0.5 * cols + sx * rows + 0.5);
+            // normalized buffer values
             unsigned int bufferx = max(0, min(bx, cols - 1));
-            unsigned int bufferz = max(0, min(bz, rows - 1));
-            buffer[bufferz * cols + bufferx] = '#';
+            unsigned int buffery = max(0, min(by, rows - 1));
+            unsigned int bufferIndex = buffery * cols + bufferx;
+            // find distance from camera to point squared
+            double distCamera = x * x + (y - cameraWall + cameraDist) * (y - cameraWall + cameraDist) + z * z;
+            if (distCamera < zbuffer[bufferIndex]) {
+                zbuffer[bufferIndex] = distCamera;
+                buffer[buffery * cols + bufferx] = '#';
+            }
         }
     }
     //  print buffer
